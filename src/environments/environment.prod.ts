35efb3d79aa42b483044bc404d154ac081524bfd
tssule3 <tssule3@gmail.com>
2018-06-28 21:29:16 +0530

    2nd commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD6S-qCQ9Lc72Unz_RXHw7q39YAKUyAfH0",
+    authDomain: "client-panel-app-final.firebaseapp.com",
+    databaseURL: "https://client-panel-app-final.firebaseio.com",
+    projectId: "client-panel-app-final",
+    storageBucket: "client-panel-app-final.appspot.com",
+    messagingSenderId: "1049040445710"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD6S-qCQ9Lc72Unz_RXHw7q39YAKUyAfH0",
+    authDomain: "client-panel-app-final.firebaseapp.com",
+    databaseURL: "https://client-panel-app-final.firebaseio.com",
+    projectId: "client-panel-app-final",
+    storageBucket: "client-panel-app-final.appspot.com",
+    messagingSenderId: "1049040445710"
+  }
 };
 
 /*
