dbc0915343e10cb767a556ba40b4578615e9a9c0
tssule3 <tssule3@gmail.com>
2018-06-29 11:07:27 +0530

    16th commit

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+prop = true;
   constructor() { }
 
   ngOnInit() {
