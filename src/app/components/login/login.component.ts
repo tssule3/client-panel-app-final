d63fb70cbc89a6e3c761bf35754c9c5a8cb4696b
tssule3 <tssule3@gmail.com>
2018-07-01 11:03:14 +0530

    34th commit

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../../services/auth.service';
+import {FlashMessagesService} from 'angular2-flash-messages';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,12 +9,32 @@ import {AuthService} from '../../services/auth.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-email;
-password;
-  constructor(private authService: AuthService) { }
+email: string;
+password: string;
+  constructor(private authService: AuthService, private flash: FlashMessagesService,
+              private router: Router) { }
 
   ngOnInit() {
+    this.authService.getAuth().subscribe(
+      auth => {if (auth) {
+        this.flash.show('Already Logged In!', {cssClass: 'alert-secondary', timeout: 1000});
+        setTimeout(() => {
+          this.router.navigate(['/']).then(
+            () => {
+            }
+          );
+        }, 1000);
+      }}
+    );
   }
   onSubmit() {
+    this.authService.login(this.email, this.password).then(
+      res => {
+        this.flash.show('Logged In', {cssClass: 'alert-success', timeout: 4000});
+        this.router.navigate(['/']);
+      }
+    ). catch(err => {
+      this.flash.show(err.message, {cssClass: 'alert-danger', timeout: 4000});
+    });
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -9,7 +9,18 @@ export class AuthService {
 
   constructor(private auth: AngularFireAuth, private router: Router,
               private flash: FlashMessagesService) { }
-  login(email, password) {
-
+  login(email: string, password: string) {
+    return new Promise(
+      (resolve, reject) => {
+        this.auth.auth.signInWithEmailAndPassword(email, password).then(
+          (userData) => resolve(userData), err => reject(err)
+        );
+      }
+    );
+  }
+  getAuth() {
+    return this.auth.authState.pipe(
+      auth => auth
+    );
   }
 }
